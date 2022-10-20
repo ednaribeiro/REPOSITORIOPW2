575d7304c2ca0ef3525362d40d27b60bc4541e2a
ednaribeiro <84679983+ednaribeiro@users.noreply.github.com>
2022-10-19 21:57:10 -0300

    subindo arquivos testes

---
+++ index.js
@@ -0,0 +1,32 @@
+/*IMPORTA O PACOTE DO EXPRESS PARA O SCRIPT index.js*/
+const express = require('express');
+const routesCategoria = require('./route/routesCategorias');
+
+/*TORNA O EXPRESS EXECUTÁVEL DENTRO DO SCRIPT inxdex.js
+ATRAVÉS DA CONSTANTE app
+*/
+const app = express();
+
+console.log('teste de github')
+
+app.use('/', routesCategoria);
+
+/** EXEMPLOS DE ROTA **/
+// app.get('/', function (req, res) {
+//     res.send('RESPOSTA DA ROTA RAIZ DA APLICAÇÃO');
+// });
+
+// app.get('/teste', function (req, res) {
+//     res.send('RESPOSTA DA ROTA TESTE DA APLICAÇÃO');
+// });
+
+/*
+CRIAR UM WEBSERVER CAPAZ DE RECEBER REQUISIÇÕES VIA
+HTTP
+PARAMETROS DO listem
+1 - PORTA LÓGICA (OBRIGATÓRIA)
+2 - CALLBACK (OPCIONAL)
+*/
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO EM - http://localhost:3000');
+});
